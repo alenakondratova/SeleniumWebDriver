b647b75d79a9d7818927851770084d027b17145f
alenakondratova <alenakondratova74@gmail.com>
2024-05-01 17:04:47 +0400

    Create MyFirstTest.java

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+    public WebDriver driver = new ChromeDriver();
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+
+       wait = new WebDriverWait(driver, Duration.ofSeconds(10)); }
+
+    @Test
+             public void myFirstTest() {
+            driver.get("http://www.google.com");
+            driver.findElement(By.name("q")).sendKeys("webdriver");
+            driver.findElement(By.name("btnG")).click();
+            wait.until(titleIs("webdriver - Поиск в Google")); }
+
+    @After
+       public void stop(){
+        driver.close();
+        driver.quit();
+            }
+        }
+
+
